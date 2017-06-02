653c82548befc8ace3eff93fe328a9da4cc1ffcd
mythrynn <jonahb3@gmail.com>
2017-06-02 09:40:32 -0600

    New Expiremental Version
    Updating GUI, adding more features.

--- main.js
+++ main.js
@@ -110,3 +110,21 @@ window.setInterval(function(){
 window.setInterval(function() {
   save();
 }, 60000);
+
+
+
+
+
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
